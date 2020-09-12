70c205a1e4fdd4d64e95a8fa8c16745ef7bfcde8
alpocnito <ron.zaripovr@gmail.com>
2020-09-12 23:31:06 +0300

    small fixes

--- 3_semestr/second_lesson/mycat.cpp
+++ 3_semestr/second_lesson/mycat.cpp
@@ -1,21 +1,80 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <errno.h> 
+#include <assert.h>
+#include <stdlib.h>
 
-const size_t READ_NUM = 100000;
+const size_t READ_NUM = 4096;
+
+ssize_t my_write(int fd, const char* buf, size_t count);
+ssize_t my_read (int fd,       char* buf, size_t count);
+
+
+ssize_t my_write(int fd, const char* buf, size_t count)
+{
+  assert(buf);
+  
+  size_t succ_write = 0;
+  while (succ_write < count)
+  {
+    ssize_t status = write(fd, buf + succ_write, count - succ_write);
+
+    if (status < 1)                //
+    {                              //
+      if (!errno || errno == EINTR)// 
+        continue;                  //
+                                   // Analysing the write errors
+      perror("my_write failed");   //
+      return -1;                   //
+    }                              //
+                                   
+    succ_write += (size_t)status;
+  }
+
+  return 0;
+}
+
+
+ssize_t my_read(int fd, char* buf, size_t count)
+{
+  size_t succ_read = 0;
+  
+  while (1)
+  {
+    ssize_t status = read(fd, buf, count);
+
+    if (status < 1)                //
+    {                              //
+      if (!errno || errno == EINTR)// 
+        continue;                  //
+                                   // Analysing the read errors
+      perror("my_read failed");    //
+      return -1;                   //
+    }                              //
+    
+    succ_read += (size_t)status;
+    break;
+  }
+    
+  return (ssize_t)succ_read;
+}
 
 int main(int argc, char** argv)
 {
   int in_fd = 0;
   int out_fd = 1;
 
+  char temp_str[READ_NUM];
+
   if (argc == 1) 
   {
-    while (temp_str[0] != EOF)
-    {  
-      if (read(in_fd, temp_str, 1) < 0)
-        perror("read problems");
-      if (write(out_fd, temp_str, 1) < 0)
-        perror("write problems");
+    while (1)
+    { 
+      ssize_t read_symbols = 0;
+      if ((read_symbols = my_read(in_fd, temp_str, READ_NUM)) < 0)
+        goto ERROR;
+      if (my_write(out_fd, temp_str, (size_t)read_symbols) < 0)
+        goto ERROR;
     }
   }/*
   else
@@ -28,5 +87,12 @@ int main(int argc, char** argv)
 
 
   }*/
+
+  free(temp_str);
   return 0;
+
+ERROR:
+  free(temp_str);
+  printf("Sorry, some error occurs\n");
+  return -1;
 }
--- 3_semestr/second_lesson/myid.cpp
+++ 3_semestr/second_lesson/myid.cpp
@@ -8,19 +8,74 @@
 #include <assert.h>
 #include <ctype.h>
 
-int PrintGroup(gid_t gid)
+
+///////////////////////////////////////////////////
+//
+// Print info about group in stdout in format: 
+//  <pw_uid(pw_name)>
+//
+// RETURN: 0 - if all is good
+//        -1 - in case of getgrgid() error
+//
+///////////////////////////////////////////////////
+int PrintGroup(gid_t gid);
+
+///////////////////////////////////////////////////
+//
+// Check is buf is number
+//
+// RETURN: false - buf it is not a number
+//         true  - buf is number
+//
+///////////////////////////////////////////////////
+bool IsNumber(char* buf);
+
+///////////////////////////////////////////////////
+//
+// Return passwd* struct by pw_name
+//
+// RETURN: passwd* - if all is okey
+//         NULL    - in case of getpwnam() error
+//
+///////////////////////////////////////////////////
+passwd* GetStructByName(const char* name);
+
+///////////////////////////////////////////////////
+//
+// Return passwd* struct by pw_uid
+//
+// RETURN: passwd* - if all is okey
+//         NULL    - in case of getpwuid error
+//
+///////////////////////////////////////////////////
+passwd* GetStructByUID(uid_t uid);
+
+///////////////////////////////////////////////////
+//
+// Print info about user in stdout in <id> linux 
+//  command style
+//
+// RETURN: true  - if all is okey
+//         false - in case of error
+//
+///////////////////////////////////////////////////
+bool GiveInfo(uid_t uid, gid_t gid);
+
+
+
+
+int PrintGroup(gid_t gid) 
 {
   printf("%u(", gid);
   
   group* gr_struct = getgrgid(gid);
-   
   if (gr_struct == NULL)
   {
-    printf("getgrgid error on [%u] group\n", gid);      
+    printf("PrintGroup(%u) error\n", gid);
     return -1;
   }
+  
   printf("%s)", gr_struct->gr_name);
-
   return 0;
 }  
 
@@ -41,8 +96,8 @@ passwd* GetStructByName(const char* name)
   passwd* pw_struct = getpwnam(name);
   if (pw_struct == NULL)
   {
-    printf("getpwuid error\n");
-    return 0;
+    printf("getpwuid(%s) error\n", name);
+    return NULL;
   }
   
   return pw_struct;
@@ -53,8 +108,8 @@ passwd* GetStructByUID(uid_t uid)
   passwd* pw_struct = getpwuid(uid);
   if (pw_struct == NULL)
   {
-    printf("getpwuid error\n");
-    return 0;
+    printf("getpwuid(%u) error\n", uid);
+    return NULL;
   }
   
   return pw_struct;
@@ -64,16 +119,13 @@ bool GiveInfo(uid_t uid, gid_t gid)
 {
   // UID
   printf("uid=%u(", uid);
-    
-  passwd* pw_struct = getpwuid(uid);
+
+  passwd* pw_struct = GetStructByUID(uid);
   if (pw_struct == NULL)
-  {
-    printf("getpwuid error\n");
     return false;
-  }
   printf("%s) ", pw_struct->pw_name);
 
-    
+  
   // GID
   printf("gid=");
   PrintGroup(gid);
@@ -81,12 +133,15 @@ bool GiveInfo(uid_t uid, gid_t gid)
 
 
   // GROUPS
-  gid_t list[NGROUPS_MAX] = {};
-    
-  int num_groups = getgroups(NGROUPS_MAX, list);
+  int ngroups = NGROUPS_MAX;
+  char* name  = pw_struct->pw_name;
+  gid_t list[NGROUPS_MAX];
+
+  int num_groups = getgrouplist(name, gid, list, &ngroups);
     
   if (num_groups == -1)
   {
+    perror("Sorry");
     printf("getgroups error\n");
     return false;
   }
@@ -100,7 +155,7 @@ bool GiveInfo(uid_t uid, gid_t gid)
     for (int i = 0; i < num_groups - 1; ++i)
     {
       putchar(',');
-      if (!PrintGroup(list[i]))
+      if (PrintGroup(list[i]))
         return false;
     }
   }
@@ -109,34 +164,49 @@ bool GiveInfo(uid_t uid, gid_t gid)
 }
   
 
+
 int main(int argc, char** argv)
 {
-    
+  // info about parameter user
   if (argc == 2)
   {
     uid_t uid = 0;
     gid_t gid = 0;
 
-    if (IsNumber(argv[1]))
-    {
-      sscanf(argv[1], "%u", &uid);
-      gid = GetStructByUID(uid)->pw_gid;
-    }
+    if (IsNumber(argv[1]))                      //
+    {                                           //
+      sscanf(argv[1], "%u", &uid);              //
+                                                //
+      passwd* pw_struct = GetStructByUID(uid);  // ./myid <number>
+      if (!pw_struct)                           //
+        goto ERROR;                             //
+                                                //
+      gid = pw_struct->pw_gid;                  //
+    }                                           //
     else 
-    {
-      uid = GetStructByName(argv[1])->pw_uid;
-      assert(uid);
-      
-      gid = GetStructByName(argv[1])->pw_gid;
-    }
-    assert(GiveInfo(uid, gid));
+    {                                               //
+      passwd* pw_struct = GetStructByName(argv[1]); //
+      if (!pw_struct)                               //
+        goto ERROR;                                 // ./myid <name>
+                                                    //
+      uid = pw_struct->pw_uid;                      //
+      gid = pw_struct->pw_gid;                      //
+    }                                               //
 
+    if (!GiveInfo(uid, gid))
+      goto ERROR;
   }
-
-  if (argc == 1)
-  {
-    assert(GiveInfo(getuid(), getgid()));
-  }
+  
+  // info about current process user
+  if (argc == 1)                        //
+  {                                     //
+    if (!GiveInfo(getuid(), getgid()))  // ./myid
+      goto ERROR;                       //
+  }                                     //
 
   return 0;
+
+ERROR:
+  printf("Some errors occurs during the program. Maybe, user with this parameters: <%s> is not exist\n", argv[1]);
+  return -1;
 }
