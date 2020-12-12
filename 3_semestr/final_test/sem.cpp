22601fc18cc508ee25adbd63f5e9bb3bf8b7ef1c
alpocnito <ron.zaripovr@gmail.com>
2020-12-12 18:33:48 +0300

    Final test!!!

--- 3_semestr/final_test/sem.cpp
+++ 3_semestr/final_test/sem.cpp
@@ -6,13 +6,15 @@
 #include <sys/sem.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <sys/shm.h>
 
 #define BOOK_PLACE 0
-#define CARS_CAN_GO 1
-#define BOATS_CAN_GO 2
+#define CAR_CAN_GO 1
+#define BOAT_CAN_GO 2
 #define SHARED_VAR 3
+#define START 4
 
-const int NUMBER_SEM = 4;
+const int NUMBER_SEM = 5;
 
 #define TRY(cmd) \
     if ((cmd) < 0) { \
@@ -20,84 +22,92 @@ const int NUMBER_SEM = 4;
       exit(EXIT_FAILURE); \
     }
 
-#define Change(SEM_NAME, VAL) do {\
-    op = {SEM_NAME, VAL, 0};  \
-    TRY( semop(semid, &op, 1) ); \
-    } while(0); \
+#define UP(SEM_NAME) do {\
+  op = {SEM_NAME, 1, 0};\
+  TRY( semop(semid, &op, 1) );\
+  } while(0); \
 
-void car(int semid, int pass_name)
+#define DOWN(SEM_NAME) do {\
+  op = {SEM_NAME, -1, 0};\
+  TRY( semop(semid, &op, 1) );\
+  } while(0); \
+
+const size_t MAX_WAIT_BOATS = 4;
+
+struct Shm {
+  size_t cur_wait_boats = 0;
+  size_t cur_wait_cars = 0;
+};
+
+
+void boat(int semid, int pass_name, Shm* shm)
 {
   sembuf op{};
+  DOWN(START);
+
+  DOWN(SHARED_VAR);
+  shm->cur_wait_boats++;
+  UP(SHARED_VAR);
+
+  PRINT_CYAN_E(N("Boat %d in the queue\n"), pass_name);
+  DOWN(BOAT_CAN_GO);
+
+  // CRITICAL
+  DOWN(BOOK_PLACE)
+  PRINT_CYAN_E(N("Boat %d on the bridge\n"), pass_name);
   
-  Change(LADDER_DOWN, 0);
-  
-  if (semctl(semid, WAS_LAST_TRIP, GETVAL))
-      break;
+  DOWN(SHARED_VAR);
+  shm->cur_wait_boats--;
+  //printf("boats: %d, cars: %d\n", shm->cur_wait_boats, shm->cur_wait_cars);
 
-    Change(NUMBER_ON_BOAT, -1);
-    
-    if (semctl(semid, WAS_LAST_TRIP, GETVAL))
-      break;
-
-    Change(NUMBER_ON_LADDER, -1);
-    PRINT_GREEN_E(N("Passangers %d on ladder\n"), pass_name);
-    Change(NUMBER_ON_LADDER, 1);
-    PRINT_GREEN_E(N("Passangers %d on boat\n"), pass_name);
-    
-    Change(VOYAGE_STARTED, 0);
-    Change(VOYAGE_ENDED, 0);
-
-    Change(LADDER_DOWN, 0);
-    Change(NUMBER_ON_LADDER, -1);
-    PRINT_GREEN_E(N("Passangers %d on ladder\n"), pass_name);
-    Change(NUMBER_ON_LADDER, 1);
-    PRINT_GREEN_E(N("Passangers %d on coast\n"), pass_name);
-    Change(NUMBER_ON_BOAT, 1);
-    Change(WANT_GO_COAST, -1);
-  PRINT_GREEN_E(BOLD("Passangers %d thanks for trip\n"), pass_name);
-}
-
-void captain(int semid, short int boat_size, short int ladder_size, int num_steps)
-{ 
-  sembuf op{};
-  Change(LADDER_DOWN, -1);
-  Change(VOYAGE_STARTED, -1);
- 
-  for (int i = 0; i < num_steps; ++i)
+  if (shm->cur_wait_boats >= MAX_WAIT_BOATS || (shm->cur_wait_cars == 0 && shm->cur_wait_boats != 0))
   {
-    if (i == num_steps - 1)
-      Change(WAS_LAST_TRIP, 1);
-
-    PRINT_RED(N("Hello stranger, welcome to our board!\n"));
-    
-    
-    Change(NUMBER_ON_BOAT, 0);
-    Change(LADDER_DOWN, 1);
-
-    PRINT_CYAN(BOLD("Waiting for clearing the ladder\n"));
-
-    Change(NUMBER_ON_LADDER, (short int)-ladder_size);
-    Change(NUMBER_ON_LADDER, ladder_size);
-
-    PRINT_MAGENTA(BOLD("Boat are full, voyage is started!!!\n")); 
-    PRINT_MAGENTA(BOLD("Voyage is ended!\n"));
-    
-    Change(VOYAGE_STARTED, 1);
-    Change(VOYAGE_ENDED, -1);
-
-    Change(LADDER_DOWN, -1);
-    
-    Change(WANT_GO_COAST, 0);
-    PRINT_CYAN(BOLD("Boat is empted!\n"));
-    Change(VOYAGE_ENDED, 1);
-    Change(WANT_GO_COAST, boat_size);
-    
-    if (i == num_steps - 1)
-      break;
-    
-    Change(VOYAGE_STARTED, -1);
+    UP(BOAT_CAN_GO);
   }
-  PRINT_RED(BOLD("Goodbye strangers!\n"));
+  else
+  {
+    UP(CAR_CAN_GO);
+  }
+  UP(SHARED_VAR);
+
+  PRINT_CYAN_E(N("Boat %d passed the bridge\n"), pass_name);
+  UP(BOOK_PLACE);
+  //CRITICAL
+}
+void car(int semid, int pass_name, Shm* shm)
+{
+  sembuf op{};
+  DOWN(START);
+
+  DOWN(SHARED_VAR);
+  shm->cur_wait_cars++;
+  UP(SHARED_VAR);
+
+  PRINT_GREEN_E(N("Car %d in the queue\n"), pass_name);
+  DOWN(CAR_CAN_GO);
+  
+  // CRITICAL
+  DOWN(BOOK_PLACE)
+  PRINT_GREEN_E(N("Car %d on the bridge\n"), pass_name);
+  
+  DOWN(SHARED_VAR);
+  shm->cur_wait_cars--;
+
+  //printf("boats: %d, cars: %d\n", shm->cur_wait_boats, shm->cur_wait_cars);
+
+  if (shm->cur_wait_boats >= MAX_WAIT_BOATS || (shm->cur_wait_cars == 0 && shm->cur_wait_boats != 0))
+  {
+    UP(BOAT_CAN_GO);
+  }
+  else
+  {
+    UP(CAR_CAN_GO);
+  }
+  UP(SHARED_VAR);
+
+  PRINT_GREEN_E(N("Car %d passed the bridge\n"), pass_name);
+  UP(BOOK_PLACE);
+  // CRITICAL
 }
 
 int main(int argc, char** argv)
@@ -106,63 +116,74 @@ int main(int argc, char** argv)
   char print_buf[BUFSIZ];
   setvbuf(stdout, print_buf, _IOLBF, BUFSIZ);
   
-  short int num_pass    = 10; 
-  short int boat_size   = 4;
-  short int ladder_size = 2;
-  int num_steps         = 5;
-  
-  if (argc >= 5)
+  if (argc != 3)
   {
-    num_pass    = (short int) atoi(argv[1]);
-    boat_size   = (short int) atoi(argv[2]);
-    ladder_size = (short int) atoi(argv[3]);
-    num_steps   = atoi(argv[4]);
+    printf("Invalid number of parameters\nDo:./sem <num_boats> <num_cars>\n");
+    exit(-1);
   }
   
+  int number_boats = atoi(argv[1]);
+  int number_cars  = atoi(argv[2]);
+  if (number_boats < 0 || number_cars < 0)
+  {
+    printf("Invalid input. Numbers must be positive\n");
+    exit(-1);
+  }
   int semid = semget(IPC_PRIVATE, NUMBER_SEM, 0777); 
   TRY( semid ); 
   sembuf op{};
 
-  Change(LADDER_DOWN, 1);
-  Change(VOYAGE_ENDED, 1);
-  Change(VOYAGE_STARTED, 1);
-  Change(NUMBER_ON_LADDER, ladder_size);
-  if (boat_size < num_pass)
-  {
-    Change(NUMBER_ON_BOAT, boat_size);
-    Change(WANT_GO_COAST, boat_size);
-  }
-  else
-  {
-    Change(NUMBER_ON_BOAT, num_pass);
-    Change(WANT_GO_COAST, num_pass);
-  }
-
-  // captain
-  pid_t cpid = fork();
-  TRY( cpid );
+  UP(SHARED_VAR);
+  UP(BOOK_PLACE);
   
-  if (cpid == 0)
-  {
-    if (boat_size < num_pass)
-      captain(semid, boat_size, ladder_size, num_steps);
-    else
-      captain(semid, num_pass, ladder_size, num_steps);
-    return 0;
-  }
+  // Open bridge
+  if (number_boats < number_cars)
+    UP(CAR_CAN_GO)
+  else
+    UP(BOAT_CAN_GO)
 
-  // passangers
-  for (int i = 0; i < num_pass; ++i)
+  // shared vars
+  int shmid = shmget(IPC_PRIVATE, sizeof(Shm), IPC_CREAT | 0777);
+  TRY(shmid);
+
+  Shm* shm = (Shm*)calloc(1, sizeof(Shm));
+  shm = (Shm*)shmat(shmid, NULL, 0);
+  if (shm == NULL || (void*)shm == (void*)-1)
+  {
+    perror("shmat");
+    exit(-1);
+  }
+  shm->cur_wait_boats = 0;
+  shm->cur_wait_cars = 0;
+
+  // make childs
+  for (int i = 0; i < number_boats; ++i)
   {
     pid_t pid = fork();
     TRY( pid );
 
     if (pid == 0)
     {
-      passanger(semid, i + 1);
+      boat(semid, i + 1, shm);
       return 0;
     }
   }
+  
+  for (int i = 0; i < number_cars; ++i)
+  {
+    pid_t pid = fork();
+    TRY( pid );
+
+    if (pid == 0)
+    {
+      car(semid, i + 1, shm);
+      return 0;
+    }
+  }
+
+  // for normal start
+  op = {START, (short int)(number_boats + number_cars), 0};
+  TRY( semop(semid, &op, 1) );
   
   return 0;
 }
