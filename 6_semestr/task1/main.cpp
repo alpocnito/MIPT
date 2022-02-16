3d94b810915e28974cff38751ce98aa11741b128
alpocnito <ron.zaripovr@gmail.com>
2022-02-16 21:07:27 +0300

    Fixes

---
+++ 6_semestr/task1/main.cpp
@@ -0,0 +1,21 @@
+#include "mpi.h"
+#include "stdio.h"
+#include "assert.h"
+
+int main()
+{
+  int size, rank;
+
+  // Инициация MPI API
+  assert(MPI_Init(NULL, NULL) == MPI_SUCCESS);
+  // Количество процессоров
+  assert(MPI_Comm_size(MPI_COMM_WORLD, &size) == MPI_SUCCESS);
+  // Номер текущего процессора
+  assert(MPI_Comm_rank(MPI_COMM_WORLD, &rank) == MPI_SUCCESS);
+
+  printf("Hello, %d of %d\n", rank, size);
+  
+  // Деинициалиция MPI API
+  assert(MPI_Finalize() == MPI_SUCCESS);
+  return 0;
+}
