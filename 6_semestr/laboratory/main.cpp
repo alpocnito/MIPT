e96137502bf9d5e3d90d621feecfb980ca90e569
alpocnito <ron.zaripovr@gmail.com>
2022-03-19 09:39:04 +0300

    laboratory

--- 6_semestr/laboratory/main.cpp
+++ 6_semestr/laboratory/main.cpp
@@ -14,6 +14,17 @@
 	#define Printf(...) ;
 #endif
 
+//#define PRINT
+
+#ifdef PRINT
+	#define Print_line(...) print_line(__VA_ARGS__);
+	#define Print_u(...) print_u(__VA_ARGS__);
+#else
+	#define Print_line(...) ;
+	#define Print_u(...) ;
+#endif
+
+
 const double X = 10;
 const double T = 10;
 
@@ -23,7 +34,7 @@ double f(double t, double x);
 double phi(double x);
 double psi(double t);
 void print_u(double** u, unsigned K, unsigned M);
-void print_line(double* u_i, unsigned K, unsigned M, unsigned i);
+void print_line(double* u_i, unsigned K, unsigned M, int i);
 
 
 int main(int argc, char** argv)
@@ -48,14 +59,18 @@ int main(int argc, char** argv)
 	if (size <= 1)
 	{
 		OneProc(K, M);
+		printf("Time: %lg\n", MPI_Wtime() - start_time);
+
   	assert(MPI_Finalize() == MPI_SUCCESS);
 		return 0;
 	}
 	
 	SeveralProc(K, M, size, rank);	
 
-	if (rank == 0) printf("Time: %lg\n", MPI_Wtime() - start_time);
-  assert(MPI_Finalize() == MPI_SUCCESS);
+	if (rank == K % size) 
+		printf("Time: %lg\n", MPI_Wtime() - start_time);
+  
+	assert(MPI_Finalize() == MPI_SUCCESS);
 	return 0;
 }
 
@@ -91,14 +106,13 @@ void OneProc(unsigned K, unsigned M)
 	for (unsigned i = 0; i <= M; ++i)
 		u[0][i] = phi(i * X / M);
 	
-	// print_u(u, K, M);
   
 	for (unsigned k = 0; k < K; ++k)
 	{
 		for (unsigned m = 1; m <= M; ++m)
 			u[k+1][m] = f(k * T/K, m * X/M) * tau + u[k][m] - (u[k][m] - u[k][m-1]) * tau / h;
 	}
-	// print_u(u, K, M);
+	Print_u(u, K, M);
 	
 	for (unsigned i = 0; i <= K; ++i)
 		free(u[i]);
@@ -112,30 +126,94 @@ void SeveralProc(unsigned K, unsigned M, unsigned size, unsigned rank)
 	assert(M > 0);
 	assert(size > 1);
 	
+	if (rank > K)
+		return;
+
+	double tau = T / K;
+	double h   = X / M;
+	
 	if (rank == 0)
 	{
 		double* u_0 = (double*)calloc(M+1, sizeof(u_0[0])); 
 		for (unsigned i = 0; i <= M; ++i)
 		{
 			u_0[i] = phi(i * X / M);
-			assert(MPI_Isend(&u_0[i], 1, MPI_DOUBLE, rank+1, i, MCW) == MPI_SUCCESS);
+			MPI_Request req;
+			assert(MPI_Isend(&u_0[i], 1, MPI_DOUBLE, int( (rank+1) % size ), int(i), MCW, &req) == MPI_SUCCESS);
 		}
-		//print_line(u_0, K, M, 0);
-		free(u_0)
+		
+		Print_line(u_0, K, M, -1);
+		Print_line(u_0, K, M, int(rank));
+		free(u_0);
+	
+		SeveralProc(K, M, size, rank + size);
 	}
 	else
 	{
-		printf("1");		
+		double* u_i = (double*)calloc(M+1, sizeof(u_i[0]));
+		u_i[0] = psi(rank * T / K);
+
+		double u_p1 = 0;
+		double u_p2 = 0;
+		assert(MPI_Recv(&u_p1, 1, MPI_DOUBLE, MPI_ANY_SOURCE, 0, MCW, MPI_STATUS_IGNORE) == MPI_SUCCESS);
+		
+		MPI_Request req;
+		assert(MPI_Isend(&u_i[0], 1, MPI_DOUBLE, int( (rank+1) % size ), 0, MCW, &req) == MPI_SUCCESS);
+
+		for (unsigned j = 1; j <= M; ++j)
+		{
+			assert(MPI_Recv(&u_p2, 1, MPI_DOUBLE, MPI_ANY_SOURCE, int(j), MCW, MPI_STATUS_IGNORE) == MPI_SUCCESS);
+			
+			u_i[j] = f((rank-1) * T/K, j * X/M) * tau + u_p2 - (u_p2 - u_p1) * tau / h;
+			
+			assert(MPI_Isend(&u_i[j], 1, MPI_DOUBLE, int( (rank+1) % size ), int(j), MCW, &req) == MPI_SUCCESS);
+
+			u_p1 = u_p2;
+		}
+
+		Print_line(u_i, K, M, int(rank));
+		free(u_i);
+
+		SeveralProc(K, M, size, rank + size);
 	}
 }
 
 
-void print_line(double* u_i, unsigned K, unsigned M, unsigned i)
+void print_line(double* u_i, unsigned K, unsigned M, int i)
 {
 	assert(u_i);
 	assert(K > 0);
 	assert(M > 0);
-	assert(i <= K);
+	assert(i <= int(K));
+
+	if (i == -1)
+	{
+		printf("\n            ");
+		for (int j = 0; j <= M; ++j)
+			printf("%-6u", j);
+		
+		printf("\n      ");
+		for (unsigned j = 0; j <= M+1; ++j)
+			printf("------");
+	
+	}
+	else if (i >= 0)
+	{
+		printf("\n%-6u", i);
+		printf("|     ");
+		
+		for (unsigned j = 0; j <= M; ++j)
+			printf("%-6.1lg", u_i[j]);
+		
+		printf("\n      |");
+	
+		if (i == K)
+			printf("\n\n\n");
+	}
+	else
+	{
+		printf("Unknown i: %d\n", i);
+	}
 }
 
 
@@ -144,15 +222,16 @@ void print_u(double** u, unsigned K, unsigned M)
 	assert(u);
 	assert(K > 0);
 	assert(M > 0);
-		
-	printf("      ");
-	for (unsigned i = 0; i <= M+1; ++i)
-		printf("------");
 	
-	printf("\nK     |M    ");
+	printf("\n            ");
 	for (int i = 0; i <= M; ++i)
 		printf("%-6u", i);
 
+	
+	printf("\n      ");
+	for (unsigned i = 0; i <= M+1; ++i)
+		printf("------");
+	
 	for (int i = 0; i <= int(K); ++i)
 	{
 		printf("\n%-6u", i);
@@ -161,7 +240,7 @@ void print_u(double** u, unsigned K, unsigned M)
 		for (unsigned j = 0; j <= M; ++j)
 			printf("%-6.1lg", u[i][j]);
 		
-		printf("\n      ");
+		printf("\n      |");
 	}
 	printf("\n\n\n");
 }
